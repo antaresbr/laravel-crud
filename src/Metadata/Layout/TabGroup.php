0fc49623db6a55c2c7e24d426994b9eddb9fe2f4
Eduardo Guerra <eduardo.antares@gmail.com>
2021-06-11 13:34:17 +0000

    Create Layout/TabGroup class.

---
+++ src/Metadata/Layout/TabGroup.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Antares\Crud\Metadata\Layout;
+
+use Antares\Crud\CrudException;
+
+class TabGroup extends AbstractContainer
+{
+    /**
+     * Class constructor
+     *
+     * @param array $data
+     * @param array $prototypes
+     */
+    public function __construct(array $data = [])
+    {
+        $data['type'] = 'tabgroup';
+
+        parent::__construct($data);
+    }
+
+    /**
+     * @see AbstractMetadata::customValidates()
+     *
+     * @return void
+     */
+    protected function customValidates()
+    {
+        $children = $this->children;
+        if (!empty($children)) {
+            foreach ($children as $child) {
+                if (!($child instanceof Tab)) {
+                    throw CrudException::forInvalidObjectType(Tab::class, $child);
+                }
+            }
+        }
+    }
+}
